69a6ccbce8d3cffb587f8506743e6860e82d5841
Vandit Jain <jainvandit11@gmail.com>
2024-05-11 06:52:21 +0530

    version2

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void HomeScreen()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+    public void PLANETS()
+    {
+        SceneManager.LoadScene("PLANETS");
+    }
+
+    public void HUMAN()
+    {
+        SceneManager.LoadScene("HUMAN");
+    }
+
+    public void MECH()
+    {
+        SceneManager.LoadScene("MECH");
+    }
+
+    public void MENUS()
+    {
+        SceneManager.LoadScene("MENUS");
+    }
+
+    public void PLANT()
+    {
+        SceneManager.LoadScene("PLANT");
+    }
+
+    public void PlanetsMenu()
+    {
+        SceneManager.LoadScene("PlanetsMenu");
+    }
+
+    public void Mercury()
+    {
+        SceneManager.LoadScene("Mercury");
+    }
+
+    public void Venus()
+    {
+        SceneManager.LoadScene("Venus");
+    }
+
+    public void Earth()
+    {
+        SceneManager.LoadScene("Earth");
+    }
+
+    public void Mars()
+    {
+        SceneManager.LoadScene("Mars");
+    }
+
+    public void Jupiter()
+    {
+        SceneManager.LoadScene("Jupiter");
+    }
+
+    public void Saturn()
+    {
+        SceneManager.LoadScene("Saturn");
+    }
+
+    public void Uranus()
+    {
+        SceneManager.LoadScene("Uranus");
+    }
+
+    public void Neptune()
+    {
+        SceneManager.LoadScene("Neptune");
+    }
+}
